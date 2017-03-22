ddab6dc02d67c4b67a0461b5ce967de92bb44a97
DnJealt <jlmtartw@avans.nl>
2017-03-22 14:38:19 +0100

    added typematchup file

---
+++ models/TypeMatchups.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'fire':{
+        'fire':0.5,
+        'water':2,
+        'grass':0.5,
+        'ground':2,
+        'rock':2,
+        'bug':0.5,
+        'fairy':0.5,
+        'ice':0.5,
+        'steel':0.5
+    }
+};
