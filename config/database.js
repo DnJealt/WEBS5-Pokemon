730a2b44fcb8cfa79e73680bf908d858b5a740ec
Ramko <rhrbooga@avans.nl>
2017-03-31 14:56:26 +0200

    database config edited

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 
 module.exports = {
 
-    'url' : 'mongodb://@localhost:27017/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://ramko:eindopdrachtw5@ds145220.mlab.com:45220/webs5db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
