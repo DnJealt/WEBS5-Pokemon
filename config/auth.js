38602a0a0bb591d61c55b5ccf20a92706d5e95ca
DnJealt <jlmtartw@avans.nl>
2017-05-19 12:10:11 +0200

    back to dev

--- config/auth.js
+++ config/auth.js
@@ -3,14 +3,14 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1772201243108734', 
         'clientSecret'  : '576c9b0e56aa206e1001527fc40ecbee',
-        'callbackURL'   : 'https://webs5-pokemon.herokuapp.com/user/auth/fb/callback'
-        // 'callbackURL'   : 'http://localhost:3000/user/auth/fb/callback'
+        //'callbackURL'   : 'https://webs5-pokemon.herokuapp.com/user/auth/fb/callback'
+        'callbackURL'   : 'http://localhost:3000/user/auth/fb/callback'
     },
     'googleAuth' : {
         'clientID'      : '646032003965-m3n49h5q1lq2b21pdcfhf268jr3v49e9.apps.googleusercontent.com',
         'clientSecret'  : 'KYHA3eKU8l0pWN8o7zSr5MQw',
-        'callbackURL'   : 'https://webs5-pokemon.herokuapp.com/user/auth/google/callback'
-        // 'callbackURL'   : 'http://localhost:3000/user/auth/google/callback'
+        //'callbackURL'   : 'https://webs5-pokemon.herokuapp.com/user/auth/google/callback'
+        'callbackURL'   : 'http://localhost:3000/user/auth/google/callback'
     }
 
 };
